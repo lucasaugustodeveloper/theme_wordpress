96d64c4a5620b313e850a9e48d087cd88abf65ee
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-21 14:15:53 -0300

    adicionado estilos e scripts do bootstrap

--- functions.php
+++ functions.php
@@ -7,6 +7,13 @@
  */
 
 function carrega_scripts() {
+//    stylesheets
 	wp_enqueue_style('general', get_template_directory_uri() . '/assets/css/general.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', array(), '3.3.7', 'all');
+
+//	javascripts
+	wp_enqueue_script('template', get_template_directory_uri() . '/assets/js/template.js', array(), '1.0.0', true);
+    wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), '3.3.7', true);
 }
 add_action('wp_enqueue_scripts', 'carrega_scripts');
