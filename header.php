207d0fa2d7c8353e2334c65fb9d876e65c246ba2
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-21 13:41:04 -0300

    adicionado função wp_head()

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,  minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Theme WordPress</title>
+	<?= wp_head() ?>
 </head>
 <body <?= body_class() ?>>
 <header class="headerMain row">
