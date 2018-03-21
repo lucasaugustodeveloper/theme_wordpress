24755a2b2c17e5efacfd05ebd2137e69d6e87e4a
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-21 13:22:04 -0300

    adiconado arquivo index no projeto

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucas Augusto
+ * Date: 21/03/2018
+ * Time: 11:52
+ */
+get_header();
+?>
+
+<div class="conteudo">
+	<main>
+		<section class="slide">
+			Slide
+		</section>
+		<section class="servicos">
+			Serviços
+		</section>
+		<section class="meio">
+			<aside class="barra-lateral">
+				Barra Lateral
+			</aside>
+			<div class="noticias">
+				Noticiais
+			</div>
+		</section>
+		<section class="mapa">
+			Mapa
+		</section>
+	</main>
+</div>
+
+<?= get_footer() ?>
